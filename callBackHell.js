1a9edf737a029f4626896c3c0dc120379b238753
Amit Kumar <144479526+Amit0022R@users.noreply.github.com>
2025-01-21 15:23:21 +0530

    call back hell

---
+++ callBackHell.js
@@ -0,0 +1,48 @@
+// Javascript ->> single threaded language , just to one thing at a time
+
+// Take piece of code inside function & we can just pass it as coll back which can be executed later point of time 
+// setTimeout( () => {
+//     console.log("Print after 5 seconds");
+// } , 5000 )
+
+//  ************************************************* \\
+
+// const cart = ["shoes" , "pants" , "kurta"];
+
+// api.createOrder( cart , () => {
+
+//     api.proceedToPayment( () => {
+
+//         api.showOrderSummary( () => {
+//             api.updateWallet();
+//         } );
+//     } ) ;
+// } )
+
+// api.showOrderSummary(); // call this api only after done with payment
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
